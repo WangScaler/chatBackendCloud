7a18108ee634a5cf2a9525122eb6524654c70c49
wangscaler <wangscaler@163.com>
2023-02-10 16:20:00 +0800

    feat(druid):完善druid并配置双数据源

--- chat-gateway/src/main/java/com/wangscaler/chatgateway/ChatGatewayApplication.java
+++ chat-gateway/src/main/java/com/wangscaler/chatgateway/ChatGatewayApplication.java
@@ -2,8 +2,9 @@ package com.wangscaler.chatgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ChatGatewayApplication {
 
     public static void main(String[] args) {
