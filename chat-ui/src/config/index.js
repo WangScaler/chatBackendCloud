b40710ac963e7924afe8d8fa46b6ee29cc91a307
wangscaler <wangscaler@163.com>
2023-07-04 17:34:53 +0800

    style(vxicon): 修改为我的微信

--- chat-ui/src/config/index.js
+++ chat-ui/src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
 	defaultRoomBg: 'https://jiangly.com/_nuxt/img/banner_111.a6be22c.gif', // 默认房间背景
 	fileUploadUrl: 'https://file.jiangly.com/api/upload/file',
-	wxAvatar: 'https://public-1300678944.cos.ap-shanghai.myqcloud.com/blog/1653965270506image.png'
+	wxAvatar: 'https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/460571bcac8248ceba5ce85a412d60ed~tplv-k3u1fbpfcp-watermark.image?'
 };
 
 export default config;
